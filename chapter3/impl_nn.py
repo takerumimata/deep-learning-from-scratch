eecba0c24f82dbd6fd698c5c08b9f2ba0c5147bb
takerumimata <fossil1908@gmail.com>
2020-07-30 15:28:31 +0900

    create file for summary for impl NN

---
+++ chapter3/impl_nn.py
@@ -0,0 +1,32 @@
+# 3.4.3 実装のまとめ - P.64
+# ３層のNNの実装 
+
+import numpy as np
+import matplotlib.pylab as plt
+import sys
+
+def init_network():
+    # ディクショナリ型変数を宣言
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.3], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+
+    return network
+
+def forward(network x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    # numpy dot演算は行列の積を求める演算
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+
+    return y
