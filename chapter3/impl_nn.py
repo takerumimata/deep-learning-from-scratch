74c862c9286d0d1cdc89db001af6967b97a59fad
takerumimata <fossil1908@gmail.com>
2020-07-30 15:35:17 +0900

    complete coding

--- chapter3/impl_nn.py
+++ chapter3/impl_nn.py
@@ -17,7 +17,7 @@ def init_network():
 
     return network
 
-def forward(network x):
+def forward(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
@@ -27,6 +27,12 @@ def forward(network x):
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
-    y = identity_function(a3)
+    # 恒等関数として本書ではidentity_functionを定義し呼び出しているが、処理的には違いはないので採用しないこととした。
+    y = a3
 
     return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
